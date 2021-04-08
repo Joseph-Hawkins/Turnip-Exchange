70b90395a8d68196be7ce07082c457984dad2184
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2021-04-08 11:26:43 -0400

    Improved scrape script
    Modified the information being scraped to include more content.

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,25 +1,42 @@
-const fetch = require("node-fetch");
-const URL = 'https://reddit.com/r/ACturnips.json'
 
-async function loadData(URL) {
+const fetch = require("node-fetch");
+const URL = 'https://reddit.com/r/ACturnips/new.json?raw_json=1'
+const Post = require('../models/Post');
+
+async function fetchPosts(URL) {
     const response = await fetch(URL);
     const posts = await response.json();
-    let post = posts.data.children[3].data
-    console.log(post.author_fullname);
-    console.log(() => { //get digit in title
-        let title = post.title
-        title = title.replace(/[^0-9]/g, '');
-        return title;
+    let post = posts.data.children[1].data //work with a group of posts not just one
+
+    //clean and display data
+    let title = post.title.slice(5)
+    let price = post.title.replace(/[^0-9]/g, '')//modify method to get price
+    let unixTime = post.created_utc
+    let link = post.id
+    //Failure if price isn't parsed
+    //console.log("Title : " + title)
+    //console.log("Price : " + price + " Bells")
+    //console.log("Link : reddit.com/" + link)
+    //console.log("Unix time of post : " + unixTime)
+
+
+    return new Post({
+        "title" : title,
+        "price" : price,
+        "link" : link
     });
-    //console.log(post)
-    console.log(post.title.replace(/[^0-9]/g, ''))
-    console.log(post.thumbnail);
-
-    //set values for posts
-
-    return posts;
 }
 
+/*(title) => {
+    if(title.replace(/[^0-9]/g, '')){
+        return title.replace(/[^0-9]/g, '');
+    }
 
+    change text to numbers
+    
+}
+*/
 
-const posts = loadData(URL);
+module.exports = {
+    posts : fetchPosts,
+}
